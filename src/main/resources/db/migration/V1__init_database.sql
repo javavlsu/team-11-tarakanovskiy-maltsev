1434c254eb2ed6782d1a4053e273edac5512e1e9
SteBullet <malzev.step@yandex.ru>
2023-06-21 21:12:50 +0300

    technical chocolates

--- src/main/resources/db/migration/V1__init_database.sql
+++ src/main/resources/db/migration/V1__init_database.sql
@@ -8,8 +8,7 @@ create table discipline
 create table group_discipline
 (
     discipline_id bigint not null,
-    group_id      bigint not null,
-    primary key (discipline_id, group_id)
+    group_id      bigint not null
 );
 
 create table student_group
@@ -48,16 +47,42 @@ create table test_question
 
 create table test_result
 (
-    id      bigint not null auto_increment,
-    score   float(53),
-    test_id bigint,
+    id         bigint not null auto_increment,
+    score      float(53),
+    student_id bigint,
+    test_id    bigint,
+    primary key (id)
+);
+
+create table test_user
+(
+    test_id bigint not null,
+    user_id bigint not null,
+    primary key (test_id, user_id)
+);
+
+create table user_role
+(
+    user_id bigint not null,
+    roles   varchar(20)
+);
+
+create table usr
+(
+    id         bigint not null auto_increment,
+    username   varchar(255),
+    password   varchar(255),
+    first_name varchar(255),
+    last_name  varchar(255),
+    patronymic varchar(255),
+    group_id   bigint,
     primary key (id)
 );
 
 alter table group_discipline
-    add constraint group_discipline_discipline_fk foreign key (discipline_id) references discipline (id);
-alter table group_discipline
     add constraint group_discipline_student_group_fk foreign key (group_id) references student_group (id);
+alter table group_discipline
+    add constraint group_discipline_discipline_fk foreign key (discipline_id) references discipline (id);
 alter table student_test
     add constraint test_discipline_fk foreign key (discipline_id) references discipline (id);
 alter table test_answer
@@ -65,4 +90,14 @@ alter table test_answer
 alter table test_question
     add constraint question_test_fk foreign key (test_id) references student_test (id);
 alter table test_result
+    add constraint result_user_fk foreign key (student_id) references usr (id);
+alter table test_result
     add constraint result_test_fk foreign key (test_id) references student_test (id);
+alter table test_user
+    add constraint test_user_user_fk foreign key (user_id) references usr (id);
+alter table test_user
+    add constraint test_user_student_test_fk foreign key (test_id) references student_test (id);
+alter table user_role
+    add constraint fk_user_role foreign key (user_id) references usr (id);
+alter table usr
+    add constraint user_group_fk foreign key (group_id) references student_group (id);
